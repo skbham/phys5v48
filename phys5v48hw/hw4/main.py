5a0102c83aa6a045a903649ed7002b24bcd8bebf
skbham <sarahkbranham@gmail.com>
2026-03-14 15:28:39 -0500

    Fix errors and consolidate main script

---
+++ phys5v48hw/hw4/main.py
@@ -0,0 +1,59 @@
+
+import argparse
+import asyncio
+import numpy as np
+import openpyxl
+import os
+import pandas as pd
+from time import perf_counter
+import tracemalloc
+import resource
+
+# import custom modules
+
+import asyncLorentz # Import the set of functions
+
+# Initialize the parser
+parser = argparse.ArgumentParser()
+
+# Add arguments to the parser
+parser.add_argument('n', type=int)
+parser.add_argument('bins', type=int)
+parser.add_argument('nP', type=int)
+parser.add_argument('nodes', type=int)
+parser.add_argument('fNameOut', type=str)
+parser.add_argument('fNameCounts', type=str)
+
+# Get the input arguments
+args = vars(parser.parse_args())	
+
+tracemalloc.start() # Start monitoring memory
+start = perf_counter() # Start timer
+
+counts = asyncLorentz.run_async(args['n'], bins=args['bins'])
+
+end = perf_counter() # Stop timer
+tracemalloc.stop() # Stop monitoring memory
+
+t = end - start # Calculate time
+
+colList = ["Problem Size (n)", "Bins", "Nodes", "Ranks", "Threads Per Rank", "Runtime", "Peak Memory"]
+
+writer = pd.ExcelWriter(args['fNameOut'], engine='openpyxl', mode='a')
+df = pd.read_excel(writer, index_col=0) # Read in catalog
+
+# Add entry to catalog
+
+#peakMem = tracemalloc.get_traced_memory()[1]
+peakMem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+newRow = [args['n'], args['bins'], args['nodes'], args['nP'], args['nP']/args['nodes'], t, peakMem]
+
+df.loc[len(df)] = newRow
+
+# Write to the catalog
+df.to_excel(excel_writer=args['fNameOut'], columns=colList, engine='openpyxl')
+
+# Save the counts to a .txt file
+np.savetxt(args['fNameCounts'], np.array(counts))
+
+
--- phys5v48hw/hw4/slurmMultiNodeTest.sh
+++ phys5v48hw/hw4/slurmMultiNodeTest.sh
@@ -19,13 +19,15 @@ source /home/biscuit/Documents/utd/phys5v48/venv/bin/activate
 n=1000
 nodes=2
 baseName="asyncLorentz"
+fNameCat="output/${baseName}/${baseName}Cat.xlsx"
+
 
 for ((nP=1; nP<17; nP*=2)); do
 for ((bins=10; bins<1001; bins*=10)); do
 
-	fNameCounts = "${baseName}CountsNP${nP}B${bins}.txt"
-	
-	python "${baseName}.py" "$n" "$bins" "$nP" "$nodes" "output/${baseName}Cat.xlsx" "output/${baseName}CountsNP${nP}Bins${bins}.txt"
+	fNameCounts="output/${baseName}/${baseName}CountsNP${nP}B${bins}.txt"
+
+	python "main.py" "$n" "$bins" "$nP" "$nodes" "$fNameCat" "$fNameCounts"
 
 done
 done
