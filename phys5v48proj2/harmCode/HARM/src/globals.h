1af39ace070c46cf97addad4057760a5805ca8c1
Sarah Branham <skb190000@g2-l-02.localdomain>
2026-05-04 22:25:13 -0500

    Add HARM code as standalone?

---
+++ phys5v48proj2/harmCode/HARM/src/globals.h
@@ -0,0 +1,9 @@
+
+
+/* these variables need to be shared between the functions
+   Utoprim_1D, residual, and utsq */
+/*
+extern FTYPE Bsq,QdotBsq,Qtsq,Qdotn,D, K_atm ;
+extern FTYPE W_for_gnr2, rho_for_gnr2, W_for_gnr2_old, rho_for_gnr2_old;
+*/
+
