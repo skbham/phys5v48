9560f593dac4a308a4baa78a18538cef89c7d7f4
skbham <sarahkbranham@gmail.com>
2026-05-11 10:03:37 -0500

    Fix TypingError NoPython

--- phys5v48hw/hw4/numbaLorentz.py
+++ phys5v48hw/hw4/numbaLorentz.py
@@ -3,8 +3,6 @@
 from numba import njit, prange, cuda
 #import atomic
 import numpy as np
-import pandas as pd
-from time import perf_counter
 
 @njit(parallel=True, nogil=True)
 
@@ -22,7 +20,6 @@ def lorentzian_histogram_numba(n, bins=100, xmin=-10, xmax=10):
         u = np.random.random() # Uniform(0,1)
         x = 1. / np.tan(np.pi * u) # x = 1/tan(pi*u)
         ix = int((x - xmin) * xfac) # Map x to bin index
-        counts = counts.astype(float)
         if 0 <= ix < bins:
             cuda.atomic.add(counts, ix, 1) # Atomic increment
 
